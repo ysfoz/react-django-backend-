2d7516a80324abb2d91037108924bdcb32864b5f
yusuf <ysfozturk3@gmail.com>
2021-01-20 23:03:06 +0100

    start be-08

--- src/users/views.py
+++ src/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -12,24 +13,10 @@ def registerView(request):
         return  Response(serializer.data)
     elif request.method == 'POST':
         serializer = RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
         
     
-
-
-
-# @api_view(['GET','POST'])
-# def post_list_create(request):
-#     if request.method == 'GET':
-#         post = Post.objects.all()
-#         serializer = PostSerializer(post,many = True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             data = {
-#                 'message':'New Post created succesfully'
-#             }
-#             return Response(data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
